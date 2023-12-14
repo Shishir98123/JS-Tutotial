b87dc165ef53fc45d037584151e28edcc6ee37a6
Shishir Khatiwada <119192456+Shishir98123@users.noreply.github.com>
2023-12-14 16:50:34 +0000

    variables and note

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 34533;
+let accountEmail = "shishirkhatiwada@gmail.com";
+var accountPassword = "12345";
+accountCity = "Dharan";
+
+accountEmail = "sk@sk.com";
+accountPassword = "23232232";
+accountCity = "Birtamode";
+
+console.log(accountId);
+console.table([accountCity, accountEmail, accountId, accountPassword]);
