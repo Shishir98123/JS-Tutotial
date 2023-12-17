bdcc09e7c18135e4d0b273c4c7adb3040a6ef1c6
Shishir Khatiwada <119192456+Shishir98123@users.noreply.github.com>
2023-12-17 09:26:28 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello");  // we are using nodejs, not browser
+
+console.log("Shishir");
+
+let name = "hitesh";
+let age = 18;
+let isLoggedIn = false;
+
+// number => 2 to power 53
+// bigint 
+// string => "";
+// boolean => true/false 
+// null => standalone value 
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Shishir Khatiwada");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
