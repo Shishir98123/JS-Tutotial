3b90162a6bd2f523d4fd7c2cc36a1d4b956eb9b6
Shishir Khatiwada <119192456+Shishir98123@users.noreply.github.com>
2023-12-18 16:13:46 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,3 +27,41 @@ let someNumber = 44;
 let changedToString = String(someNumber);
 console.log(changedToString);
 console.log(typeof changedToString);
+
+
+// ********************* OPERATIONS ******************* //
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+//console.log(2%3);
+
+let str1 = "hello";
+let str2 = "shishir";
+
+let str3 = str1 + str2;
+console.log(str3);
+
+//console.log ("1" + 2);
+//console.log (1 + "2");
+//console.log("1" + 2 + 3);
+//console.log(1 + 3 + "4");
+
+//console.log((3 + 4) * 5 / 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
